8eb006f8f3f66c47772eba80f618d966e4a311b6
Md. Minhajul Hoque <md.minhajulhoque@Mds-Mac-mini.local>
2026-05-01 01:03:17 +0600

    fix google analytics env

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,10 +33,9 @@ export default async function RootLayout({
             dangerouslySetInnerHTML={{ __html: JSON.stringify(entry) }}
           />
         ))}
-        {process.env.NODE_ENV === 'production' &&
-          process.env.NEXT_PUBLIC_GA_ID_PUBLIC && (
-            <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID_PUBLIC} />
-          )}
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+        )}
       </body>
     </html>
   );
